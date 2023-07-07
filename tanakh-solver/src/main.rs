77009063cf60846999e5f2549381323232e54ea2
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2023-07-07 22:39:43 +0900

    split api code

--- tanakh-solver/src/main.rs
+++ tanakh-solver/src/main.rs
@@ -1,74 +1,7 @@
 use anyhow::Result;
 use rand::Rng;
-use serde::{Deserialize, Serialize};
-use serde_json::json;
 
-#[derive(Deserialize, Debug)]
-struct Problem {
-    room_width: f64,
-    room_height: f64,
-    stage_width: f64,
-    stage_height: f64,
-    stage_bottom_left: Vec<f64>,
-    musicians: Vec<usize>,
-    attendees: Vec<Atendee>,
-}
-
-#[derive(Deserialize, Debug)]
-struct Atendee {
-    x: f64,
-    y: f64,
-    tastes: Vec<f64>,
-}
-
-#[derive(Serialize)]
-struct Solution {
-    placements: Vec<Placement>,
-}
-
-#[derive(Serialize)]
-struct Placement {
-    x: f64,
-    y: f64,
-}
-
-#[derive(Deserialize, Debug)]
-enum Response<T> {
-    Success(T),
-    Failure(String),
-}
-
-const ENDPOINT: &str = "https://api.icfpcontest.com";
-
-fn get_problem(problem_id: u32) -> Result<Problem> {
-    let url = format!("{ENDPOINT}/problem?problem_id={problem_id}");
-    let resp: Response<String> = reqwest::blocking::get(&url)?.json()?;
-    match resp {
-        Response::Success(s) => Ok(serde_json::from_str(&s)?),
-        Response::Failure(s) => anyhow::bail!("Failure: {s}"),
-    }
-}
-
-#[derive(Debug)]
-struct SubmitId(String);
-
-fn submit(problem_id: u32, solution: &[Placement]) -> Result<SubmitId> {
-    let url = format!("{ENDPOINT}/submission");
-    let client = reqwest::blocking::Client::new();
-    let token = std::env::var("API_TOKEN")?;
-    let resp: String = dbg!(client
-        .post(&url)
-        .bearer_auth(token)
-        .json(&json!({
-            "problem_id": problem_id,
-            "contents": serde_json::to_string(&json!({
-                "placements": solution
-            }))?
-        }))
-        .send()?)
-    .text()?;
-    Ok(SubmitId(resp))
-}
+use tanakh_solver::api::{get_problem, submit, Placement};
 
 fn main() -> Result<()> {
     let problem_id = 42;
