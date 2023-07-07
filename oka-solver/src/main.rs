533340f849a0106474609682f6f3b88d146ee696
Keigo Oka <ogiekako@gmail.com>
2023-07-08 08:25:57 +0900

    Random hill climbing solver

--- oka-solver/src/main.rs
+++ oka-solver/src/main.rs
@@ -1,47 +1,45 @@
+mod solver;
+
 use anyhow::Result;
-use common::{board::Board, evaluate, Problem, Solution};
+use common::{api::Client, board::Board, evaluate, Problem, Solution};
 use lyon_geom::Point;
 use rand::{rngs::StdRng, Rng, SeedableRng};
+
+use crate::solver::Solver;
 
 #[argopt::cmd]
 fn main(
     /// time limit in seconds
-    #[opt(long, default_value = "10.0")]
-    time_limit: f64,
+    #[opt(long, default_value = "5")]
+    time_limit: u64,
     /// number of threads
     #[opt(long, default_value = "1")]
     threads: usize,
     /// problem id
     problem_id: u32,
 ) -> Result<()> {
-    let mut rng = StdRng::seed_from_u64(42);
+    let cl = Client::new();
+    let userboard = cl.get_userboard()?;
+
+    let best_score = userboard.problems[(problem_id - 1) as usize].unwrap_or(0.);
+
+    println!("our best score: {}", best_score);
 
     let problem = Problem::read_from_file(format!("problems/{}.json", problem_id))?;
 
-    let mut board = Board::new(problem_id, problem.clone());
+    let solver = Solver::new(problem_id, problem, time_limit as u64, 42);
 
-    for i in 0..board.prob.musicians.len() {
-        loop {
-            let x: f64 = rng.gen_range(board.prob.stage.min.x..board.prob.stage.max.x);
-            let y: f64 = rng.gen_range(board.prob.stage.min.y..board.prob.stage.max.y);
-            if board.try_place(i, Point::new(x, y)).is_ok() {
-                break;
-            }
-        }
+    let (score, board) = solver.solve();
+
+    println!("final score: {}", score);
+
+    let solution: Solution = board.try_into().unwrap();
+
+    if score > best_score {
+        cl.post_submission(problem_id, solution)?;
+
+        println!("Submitted solution for problem {}!", problem_id);
     }
-
-    println!("score: {}", board.score());
-
-    let solution: Solution = board.clone().try_into().unwrap();
-
-    let actual_score = evaluate(&problem, &solution);
-    println!("actual score: {}", actual_score);
-
-    for i in 0..problem.musicians.len() {
-        board.unplace(i);
-    }
-
-    assert_eq!(board.score(), 0.0);
 
     Ok(())
 }
