353fba4c87e17a0731a0c8e1796fa2fb7f2c2f2f
Shuhei Takahashi <nya@google.com>
2023-07-08 16:38:17 +0900

    backend: Annotate solutions with submissions

--- backend/internal/worker/worker.go
+++ backend/internal/worker/worker.go
@@ -43,14 +43,14 @@ type worker struct {
 	db     *database.DB
 	client *official.Client
 
-	knownSubmissionIDs map[string]struct{}
+	finishedSubmissionIDs map[string]struct{}
 }
 
 func newWorker(db *database.DB, client *official.Client) *worker {
 	return &worker{
-		db:                 db,
-		client:             client,
-		knownSubmissionIDs: make(map[string]struct{}),
+		db:                    db,
+		client:                client,
+		finishedSubmissionIDs: make(map[string]struct{}),
 	}
 }
 
@@ -61,33 +61,42 @@ func (w *worker) DownloadSubmissions(ctx context.Context) error {
 	}
 
 	for _, submission := range submissions {
-		if _, ok := w.knownSubmissionIDs[submission.ID]; ok {
+		if _, ok := w.finishedSubmissionIDs[submission.ID]; ok {
 			continue
 		}
 
-		if _, err := w.db.GetSolutionBySubmissionID(ctx, submission.ID); err == nil {
-			w.knownSubmissionIDs[submission.ID] = struct{}{}
-			continue
-		} else if !errors.Is(err, sql.ErrNoRows) {
+		// Consider creating a solution.
+		var solutionUUID string
+		if solution, err := w.db.GetSolutionBySubmissionID(ctx, submission.ID); err == nil {
+			if solution.Submission.State == "FINISHED" {
+				// No need to process again.
+				w.finishedSubmissionIDs[submission.ID] = struct{}{}
+				continue
+			}
+			solutionUUID = solution.UUID
+		} else if errors.Is(err, sql.ErrNoRows) {
+			log.Printf("Creating a solution for submission %s", submission.ID)
+
+			spec, err := w.client.GetSubmissionSpec(ctx, submission.ID)
+			if err != nil {
+				return err
+			}
+
+			spec, err = patchSolutionSpec(spec, submission.ProblemID)
+			if err != nil {
+				return err
+			}
+
+			solutionUUID, err = w.db.SubmitSolution(ctx, submission.ProblemID, spec)
+			if err != nil {
+				return err
+			}
+		} else if err != nil {
 			return err
 		}
 
-		log.Printf("Downloading submission %s", submission.ID)
-
-		spec, err := w.client.GetSubmissionSpec(ctx, submission.ID)
-		if err != nil {
-			return err
-		}
-
-		spec, err = patchSolutionSpec(spec, submission.ProblemID)
-		if err != nil {
-			return err
-		}
-
-		solutionUUID, err := w.db.SubmitSolution(ctx, submission.ProblemID, spec)
-		if err != nil {
-			return err
-		}
+		// Create or update a submission.
+		log.Printf("Updating submission %s", submission.ID)
 
 		created, err := time.Parse("2006-01-02T15:04:05.999999999Z", submission.SubmittedAt)
 		if err != nil {
@@ -112,7 +121,9 @@ func (w *worker) DownloadSubmissions(ctx context.Context) error {
 			return err
 		}
 
-		w.knownSubmissionIDs[submission.ID] = struct{}{}
+		if submission.Done {
+			w.finishedSubmissionIDs[submission.ID] = struct{}{}
+		}
 	}
 
 	return nil
