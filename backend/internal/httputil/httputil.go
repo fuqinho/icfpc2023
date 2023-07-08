e7657e8f827787c58bb5c9805c1b9a61f9e51738
Shuhei Takahashi <nya@google.com>
2023-07-08 15:54:57 +0900

    backend: Introduce worker

--- backend/internal/httputil/httputil.go
+++ backend/internal/httputil/httputil.go
@@ -8,9 +8,17 @@ import (
 )
 
 func GetJSON(ctx context.Context, url string, out interface{}) error {
+	return GetJSONWithAuth(ctx, url, "", out)
+}
+
+func GetJSONWithAuth(ctx context.Context, url string, token string, out interface{}) error {
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
 		return fmt.Errorf("%s: %w", url, err)
+	}
+
+	if token != "" {
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
 	}
 
 	res, err := http.DefaultClient.Do(req)
