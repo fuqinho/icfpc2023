2e89370b164528a1e59dfdd624894398c28e59cf
phoenixstarhiro <phoenixstarhiro@users.noreply.github.com>
2023-07-08 22:07:55 +0900

    support estimate

--- common/src/evaluate.rs
+++ common/src/evaluate.rs
@@ -1,3 +1,4 @@
+use euclid::default::{Box2D, Point2D, Vector2D};
 use lyon_geom::LineSegment;
 
 use crate::problem::Attendee;
@@ -59,4 +60,126 @@ pub fn evaluate(problem: &Problem, solution: &Solution) -> f64 {
         .iter()
         .map(|attendee| evaluate_attendee(attendee, &problem.musicians, solution))
         .sum()
+}
+
+// Find rough upper bound.
+pub fn estimate(problem_id: u32, problem: &Problem) -> (f64, Solution) {
+    let p1 = Point2D::new(problem.stage.min.x + 10., problem.stage.min.y + 10.);
+    let p2 = Point2D::new(problem.stage.min.x + 10., problem.stage.max.y - 10.);
+    let p3 = Point2D::new(problem.stage.max.x - 10., problem.stage.max.y - 10.);
+    let p4 = Point2D::new(problem.stage.max.x - 10., problem.stage.min.y + 10.);
+
+    let s1 = LineSegment { from: p1, to: p2 };
+    let s2 = LineSegment { from: p2, to: p3 };
+    let s3 = LineSegment { from: p3, to: p4 };
+    let s4 = LineSegment { from: p4, to: p1 };
+
+    let mut candidates = vec![];
+    for a in problem.attendees.iter() {
+        let mut min_dist_sq = f64::MAX;
+        let mut candidate = Point2D::new(0., 0.);
+        for s in [&s1, &s2, &s3, &s4] {
+            let p = s.closest_point(a.position);
+            let dist_sq = (p - a.position).square_length();
+            if min_dist_sq > dist_sq {
+                min_dist_sq = dist_sq;
+                candidate = p;
+            }
+        }
+        candidates.push(candidate);
+    }
+    candidates.push(problem.stage.center());
+
+    let mut placements = vec![];
+    for (idx, inst) in problem.musicians.iter().enumerate() {
+        let mut pos = Point2D::new(0., 0.);
+        let mut max_score = f64::MIN;
+        for candidate in candidates.iter() {
+            let mut score = 0.;
+            for a in problem.attendees.iter() {
+                score += (1_000_000f64 * a.tastes[*inst]
+                    / LineSegment {
+                        from: a.position,
+                        to: *candidate,
+                    }
+                    .square_length())
+                .ceil();
+            }
+            if max_score < score {
+                max_score = score;
+                pos = *candidate;
+            }
+        }
+
+        if placements.iter().any(|p: &Placement| {
+            LineSegment {
+                from: p.position,
+                to: pos,
+            }
+            .square_length()
+                < 100.
+        }) {
+            // conflict.
+            let box2d = Box2D {
+                min: problem.stage.min + Vector2D::new(10., 10.),
+                max: problem.stage.max - Vector2D::new(10., 10.),
+            };
+            let mut found = false;
+            for i in (1..problem.musicians.len() as isize) {
+                let mut max_score = f64::MIN;
+                let mut max_pos = Point2D::new(0., 0.);
+                for ix in -i..=i {
+                    for iy in -i..=i {
+                        let candidate = pos + Vector2D::new((10 * ix) as f64, (10 * iy) as f64);
+                        if candidate.x < box2d.min.x
+                            || box2d.max.x < candidate.x
+                            || candidate.y < box2d.min.y
+                            || box2d.min.y < candidate.y
+                        {
+                            continue;
+                        }
+                        if placements.iter().any(|p: &Placement| {
+                            LineSegment {
+                                from: p.position,
+                                to: candidate,
+                            }
+                            .square_length()
+                                < 100.
+                        }) {
+                            continue;
+                        }
+                        let mut score = 0.;
+                        for a in problem.attendees.iter() {
+                            score += (1_000_000f64 * a.tastes[*inst]
+                                / LineSegment {
+                                    from: a.position,
+                                    to: candidate,
+                                }
+                                .square_length())
+                            .ceil();
+                        }
+                        if max_score < score {
+                            max_score = score;
+                            max_pos = candidate;
+                            found = true;
+                        }
+                    }
+                }
+                if found {
+                    pos = max_pos;
+                    break;
+                }
+            }
+            if !found {
+                panic!("cannot avoided");
+            }
+        }
+        placements.push(Placement { position: pos });
+    }
+    let solution = Solution {
+        problem_id,
+        placements,
+    };
+    let score = evaluate(problem, &solution);
+    (score, solution)
 }
