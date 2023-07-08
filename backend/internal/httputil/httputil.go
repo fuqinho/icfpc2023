897e16a4ddba3944bd2d08b5c69074f30ec29093
Shuhei Takahashi <nya@google.com>
2023-07-08 13:22:40 +0900

    backend: Update problems

--- backend/internal/httputil/httputil.go
+++ backend/internal/httputil/httputil.go
@@ -10,21 +10,21 @@ import (
 func GetJSON(ctx context.Context, url string, out interface{}) error {
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
-		return err
+		return fmt.Errorf("%s: %w", url, err)
 	}
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return err
+		return fmt.Errorf("%s: %w", url, err)
 	}
 	defer res.Body.Close()
 
 	if res.StatusCode/100 != 2 {
-		return fmt.Errorf("HTTP status %d", res.StatusCode)
+		return fmt.Errorf("%s: HTTP status %d", url, res.StatusCode)
 	}
 
 	if err := json.NewDecoder(res.Body).Decode(out); err != nil {
-		return err
+		return fmt.Errorf("%s: %w", url, err)
 	}
 
 	return nil
