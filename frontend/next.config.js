0c95ff9a425759e3a67c689ac180120feb2d4cfd
Shuhei Takahashi <nya@google.com>
2023-07-09 10:50:41 +0900

    frontend: Fix WASM unavailable on server

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,8 +4,17 @@ const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  webpack: (config) => {
+  webpack: (config, options) => {
     config.experiments.asyncWebAssembly = true;
+
+    // https://github.com/vercel/next.js/issues/25852
+    config.optimization.moduleIds = 'named';
+    if (options.isServer) {
+        config.output.webassemblyModuleFilename = './../static/wasm/[modulehash].wasm';
+    } else {
+        config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
+    }
+
     config.plugins.push(
       new WasmPackPlugin({
         crateDirectory: path.resolve(__dirname, "../wasm"),
