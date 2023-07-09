bc479be4e63c6440e7765d3798656503271e17cd
Shuhei Takahashi <nya@google.com>
2023-07-09 20:12:22 +0900

    wasm: Update

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -2,7 +2,51 @@ mod annealing;
 
 pub use annealing::*;
 
-use wasm_bindgen::prelude::wasm_bindgen;
+use wasm_bindgen::{prelude::wasm_bindgen, JsError, JsValue};
+
+/// Wraps [`anyhow::Error`] for conversion to [`JsValue`].
+#[derive(thiserror::Error, Debug)]
+#[error(transparent)]
+pub struct Error(#[from] anyhow::Error);
+
+impl From<Error> for JsValue {
+    fn from(value: Error) -> JsValue {
+        JsValue::from(JsError::from(value))
+    }
+}
+
+/// Specialized [`std::result::Result`] for [`Error`].
+type Result<T> = std::result::Result<T, Error>;
+
+#[wasm_bindgen]
+pub struct ProblemHandle {
+    problem: common::Problem,
+}
+
+#[wasm_bindgen]
+impl ProblemHandle {
+    pub fn from_json(json: &str) -> Result<ProblemHandle> {
+        let problem = common::RawProblem::from_json(json)?;
+        Ok(ProblemHandle {
+            problem: problem.into(),
+        })
+    }
+}
+
+#[wasm_bindgen]
+pub struct SolutionHandle {
+    solution: common::Solution,
+}
+
+#[wasm_bindgen]
+impl SolutionHandle {
+    pub fn from_json(json: &str) -> Result<SolutionHandle> {
+        let solution = common::RawSolution::from_json(json)?;
+        Ok(SolutionHandle {
+            solution: solution.into(),
+        })
+    }
+}
 
 #[wasm_bindgen]
 pub struct Evaluator {}
