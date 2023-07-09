4c192deb05f6cd8f74b610da3e26e1d973e1af5b
Keigo Oka <ogiekako@gmail.com>
2023-07-09 18:55:45 +0900

    board: add util

--- greedy-solver/src/main.rs
+++ greedy-solver/src/main.rs
@@ -10,52 +10,54 @@ mod solver;
 use anyhow::Result;
 use common::{api::Client, evaluate, Solution};
 
+use crate::solver::Solver;
+
 // use crate::solver::Solver;
 
 #[argopt::cmd]
 fn main(problem_id: u32, #[opt(short, long, default_value = "")] out: String) -> Result<()> {
-    // let cl = Client::new();
-    // let userboard = cl.get_userboard()?;
+    let cl = Client::new();
+    let userboard = cl.get_userboard()?;
 
-    // let best_score = userboard.problems[(problem_id - 1) as usize].unwrap_or(0.);
+    let best_score = userboard.problems[(problem_id - 1) as usize].unwrap_or(0.);
 
-    // eprintln!("our best score: {}", best_score);
+    eprintln!("our best score: {}", best_score);
 
-    // let problem = cl.get_problem(problem_id)?;
+    let problem = cl.get_problem(problem_id)?;
 
-    // let mut solver = Solver::new(problem_id, problem.clone());
+    let mut solver = Solver::new(problem_id, problem.clone());
 
-    // let (score, board) = solver.solve();
+    let (score, board) = solver.solve();
 
-    // let solution: Solution = board.try_into().unwrap();
+    let solution: Solution = board.try_into().unwrap();
 
-    // let eval_score = evaluate(&problem, &solution);
+    let eval_score = evaluate(&problem, &solution);
 
-    // eprintln!("final score: {}", eval_score);
+    eprintln!("final score: {}", eval_score);
 
-    // if score != eval_score {
-    //     let diff = (1. - score / eval_score).abs() * 100.;
-    //     eprintln!(
-    //         "WARNING: board and evaluate score differ by {}% {}",
-    //         diff,
-    //         if score < eval_score {
-    //             "board score is smaller"
-    //         } else {
-    //             "board score is larger"
-    //         }
-    //     );
-    // }
+    if score != eval_score {
+        let diff = (1. - score / eval_score).abs() * 100.;
+        eprintln!(
+            "WARNING: board and evaluate score differ by {}% {}",
+            diff,
+            if score < eval_score {
+                "board score is smaller"
+            } else {
+                "board score is larger"
+            }
+        );
+    }
 
-    // if eval_score > best_score {
-    //     cl.post_submission(problem_id, solution.clone())?;
+    if eval_score > best_score {
+        cl.post_submission(problem_id, solution.clone())?;
 
-    //     eprintln!("Submitted solution for problem {}!", problem_id);
-    // }
+        eprintln!("Submitted solution for problem {}!", problem_id);
+    }
 
-    // if out != "" {
-    //     eprintln!("Writing solution to {}", out);
-    //     Solution::write_to_file(out, solution)?;
-    // }
+    if out != "" {
+        eprintln!("Writing solution to {}", out);
+        Solution::write_to_file(out, solution)?;
+    }
 
     Ok(())
 }
--- greedy-solver/src/solver.rs
+++ greedy-solver/src/solver.rs
@@ -1,78 +1,124 @@
-// use common::{board::Board, Problem};
-// use lyon_geom::Point;
+use common::{board::Board, Problem};
+use lyon_geom::Point;
 
-// pub struct Solver {
-//     num_musicians: usize,   // num musicians (m)
-//     num_attendees: usize,   // num attendees (a)
-//     num_instruments: usize, // num instruments (i)
+pub struct Solver {
+    num_musicians: usize,   // num musicians (m)
+    num_attendees: usize,   // num attendees (a)
+    num_instruments: usize, // num instruments (i)
 
-//     // m -> maximal points sorted by score. (smaller first)
-//     maximal_points: Vec<Vec<(f64, P)>>,
+    // i -> maximal points sorted by score. (smaller first)
+    maximal_points: Vec<Vec<(f64, P)>>,
 
-//     board: Board,
-// }
+    board: Board,
+}
 
-// type P = Point<f64>;
+type P = Point<f64>;
 
-// const D: usize = 10;
+const D: usize = 10;
 
-// impl Solver {
-//     pub fn new(problem_id: u32, problem: Problem) -> Self {
-//         let board = Board::new(problem_id, problem, "greedy-solver");
+impl Solver {
+    pub fn new(problem_id: u32, problem: Problem) -> Self {
+        let board = Board::new(problem_id, problem, "greedy-solver");
 
-//         let num_musicians = board.prob.musicians.len();
-//         let num_attendees = board.prob.attendees.len();
-//         let num_instruments = board.prob.attendees[0].tastes.len();
+        let num_musicians = board.prob.musicians.len();
+        let num_attendees = board.prob.attendees.len();
+        let num_instruments = board.prob.attendees[0].tastes.len();
 
-//         let maximal_points = vec![];
+        let maximal_points = vec![];
 
-//         Self {
-//             num_musicians,
-//             num_attendees,
-//             num_instruments,
-//             maximal_points,
-//             board,
-//         }
-//     }
+        Self {
+            num_musicians,
+            num_attendees,
+            num_instruments,
+            maximal_points,
+            board,
+        }
+    }
 
-//     fn prob(&self) -> &Problem {
-//         &self.board.prob
-//     }
+    fn prob(&self) -> &Problem {
+        &self.board.prob
+    }
 
-//     fn compute_maximal_points(&mut self) {
-//         let mut maximal_points = vec![vec![]; self.num_musicians];
+    fn top(&self) -> f64 {
+        self.board.prob.stage.max.y
+    }
 
-//         for i in 0..self.num_musicians {
-//             let musician = self.prob().musicians[i];
+    fn bottom(&self) -> f64 {
+        self.board.prob.stage.min.y
+    }
 
-//             for a in 0..self.num_attendees {
-//                 let attendee = self.prob().attendees[a];
+    fn right(&self) -> f64 {
+        self.board.prob.stage.max.x
+    }
 
-//                 let mut score = 0.;
+    fn left(&self) -> f64 {
+        self.board.prob.stage.min.x
+    }
 
-//                 for j in 0..self.num_instruments {
-//                     let taste = attendee.tastes[j];
-//                     let musician_has = musician.has[j];
+    fn is_on_stage(&self, p: Point<f64>) -> bool {
+        p.x >= self.left() && p.x <= self.right() && p.y >= self.bottom() && p.y <= self.top()
+    }
 
-//                     if taste && musician_has {
-//                         score += 1.;
-//                     }
-//                 }
+    fn init_maximal_points(&mut self) {
+        let mut maximal_points = vec![vec![]; self.num_instruments];
 
-//                 maximal_points[i].push((score, attendee.pos));
-//             }
+        for i in 0..self.num_instruments {
+            let m = self.board.available_musician_with_instrument(i).unwrap();
 
-//             maximal_points[i].sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
-//         }
+            let delta = 0.125;
 
-//         self.maximal_points = maximal_points;
-//     }
+            for (dx, dy, x, y) in [
+                (delta, 0., 0., self.bottom()),
+                (0., delta, self.left(), 0.),
+                (delta, 0., 0., self.top()),
+                (0., delta, self.right(), 0.),
+            ] {
+                let mut graph = vec![];
 
-//     pub fn solve(&mut self) -> (f64, Board) {
-//         let mut scores = vec![vec![]];
+                let mut p = Point::new(x, y);
 
-//         // for i in self.prob.
+                while self.is_on_stage(p) {
+                    let score = self.board.score_increase_if_put_musician_on(m, p).unwrap();
 
-//         todo!()
-//     }
-// }
+                    graph.push((score, p));
+
+                    p.x += dx;
+                    p.y += dy;
+                }
+
+                for i in 0..graph.len() {
+                    if i > 0 && graph[i].0 < graph[i - 1].0 {
+                        continue;
+                    }
+                    if i + 1 < graph.len() && graph[i].0 <= graph[i + 1].0 {
+                        continue;
+                    }
+                    maximal_points[i].push(graph[i]);
+                }
+            }
+
+            maximal_points[i].sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+        }
+
+        self.maximal_points = maximal_points;
+    }
+
+    // pub fn musician_for_instrument(&mut self, ins: usize) -> Option<usize> {
+    //     if self.unavailable[ins] {
+    //         return None;
+    //     }
+    //     let res = self.board.available_musician_with_instrument(ins);
+    //     if res.is_none() {
+    //         self.unavailable[ins] = true;
+    //     }
+    //     res
+    // }
+
+    pub fn solve(&mut self) -> (f64, Board) {
+        self.init_maximal_points();
+
+        // Put all the points greedily.
+
+        todo!()
+    }
+}
