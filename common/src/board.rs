992464236fadabb22a2366473e770346408bdba2
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2023-07-09 17:08:24 +0900

    performance tuning

--- common/src/board.rs
+++ common/src/board.rs
@@ -207,60 +207,53 @@ impl Board {
                 continue;
             }
 
-            if let Some((q, r)) = q {
-                for rev in [false, true] {
-                    let (blocking, blocked, _, i, _, r, _, aids) = if rev {
-                        (*q, p, i, m, *r, MUSICIAN_R, &aids[i], &aids[m])
-                    } else {
-                        (p, *q, m, i, MUSICIAN_R, *r, &aids[m], &aids[i])
-                    };
-                    if i >= prob.musicians.len() {
-                        // blocked is pillar, we don't need to calculate impact
-                        continue;
-                    }
+            let Some((q, r)) = q else { continue; };
 
-                    // Update for blocked musician.
-
-                    let (t1, t2) = tangent_to_circle(blocked, blocking, r);
-
-                    let mut r1: f64 = (t1 - blocked).angle_from_x_axis().radians;
-                    let mut r2: f64 = (t2 - blocked).angle_from_x_axis().radians;
-
-                    let eps = 1e-12;
-                    r1 += eps;
-                    r2 -= eps;
-
-                    if r1 < r2 {
-                        let j1 = aids.partition_point(|r| r.0 < r1);
-                        let j2 = aids[j1..].partition_point(|r| r.0 < r2) + j1;
-
-                        for j in j1..j2 {
-                            if inc {
-                                Self::inc_blocks(blocks, impacts, prob, ps, i, aids[j].1);
-                            } else {
-                                Self::dec_blocks(blocks, impacts, prob, ps, i, aids[j].1);
-                            }
-                        }
-                    } else {
-                        let j2 = aids.partition_point(|r| r.0 < r2);
-                        let j1 = aids[j2..].partition_point(|r| r.0 < r1) + j2;
-
-                        for j in 0..j2 {
-                            if inc {
-                                Self::inc_blocks(blocks, impacts, prob, ps, i, aids[j].1);
-                            } else {
-                                Self::dec_blocks(blocks, impacts, prob, ps, i, aids[j].1);
-                            }
-                        }
-                        for j in j1..aids.len() {
-                            if inc {
-                                Self::inc_blocks(blocks, impacts, prob, ps, i, aids[j].1);
-                            } else {
-                                Self::dec_blocks(blocks, impacts, prob, ps, i, aids[j].1);
-                            }
-                        }
-                    };
+            for rev in [false, true] {
+                let (blocking, blocked, _, i, _, r, _, aids) = if rev {
+                    (*q, p, i, m, *r, MUSICIAN_R, &aids[i], &aids[m])
+                } else {
+                    (p, *q, m, i, MUSICIAN_R, *r, &aids[m], &aids[i])
+                };
+                if i >= prob.musicians.len() {
+                    // blocked is pillar, we don't need to calculate impact
+                    continue;
                 }
+
+                // Update for blocked musician.
+
+                let (t1, t2) = tangent_to_circle(blocked, blocking, r);
+
+                let mut r1: f64 = (t1 - blocked).angle_from_x_axis().radians;
+                let mut r2: f64 = (t2 - blocked).angle_from_x_axis().radians;
+
+                let eps = 1e-12;
+                r1 += eps;
+                r2 -= eps;
+
+                if r1 < r2 {
+                    let j1 = aids.partition_point(|r| r.0 < r1);
+                    let j2 = aids[j1..].partition_point(|r| r.0 < r2) + j1;
+
+                    for (_, a) in &aids[j1..j2] {
+                        if inc {
+                            Self::inc_blocks(blocks, impacts, prob, ps, i, *a);
+                        } else {
+                            Self::dec_blocks(blocks, impacts, prob, ps, i, *a);
+                        }
+                    }
+                } else {
+                    let j2 = aids.partition_point(|r| r.0 < r2);
+                    let j1 = aids[j2..].partition_point(|r| r.0 < r1) + j2;
+
+                    for (_, a) in aids[0..j2].iter().chain(aids[j1..].iter()) {
+                        if inc {
+                            Self::inc_blocks(blocks, impacts, prob, ps, i, *a);
+                        } else {
+                            Self::dec_blocks(blocks, impacts, prob, ps, i, *a);
+                        }
+                    }
+                };
             }
         }
     }
