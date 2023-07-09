b8a0fb7c215f9064c22d5f815ce67397d671657c
Masaya Suzuki <draftcode@gmail.com>
2023-07-08 21:26:41 -0700

    Show total score

--- frontend/app/Home.tsx
+++ frontend/app/Home.tsx
@@ -24,9 +24,11 @@ import { orderBy } from "natural-orderby";
 function ProblemListItem({
   problem,
   bestSolution,
+  totalScore,
 }: {
   problem: ProblemMetadata;
   bestSolution: SolutionMetadata | undefined;
+  totalScore: number;
 }) {
   const { data: problemSpec, error: errorProblemSpec } = useProblemSpec(
     problem.id,
@@ -68,6 +70,18 @@ function ProblemListItem({
           </div>
         </div>
       </td>
+      <td className="text-mono">
+        <div className="stat">
+          <div className="stat-value">
+            {(
+              (bestSolution?.submission?.score ?? 0) / totalScore
+            ).toLocaleString(undefined, {
+              style: "percent",
+              minimumFractionDigits: 2,
+            })}
+          </div>
+        </div>
+      </td>
     </tr>
   );
 }
@@ -88,6 +102,8 @@ function ProblemList() {
   if (!problems || bestSolutions.size == 0) {
     return <div>Loading...</div>;
   }
+  let totalScore = 0;
+  bestSolutions.forEach((v) => (totalScore += v.submission?.score ?? 0));
 
   let problemKeys = Array.from(problems.keys());
   if (!showV1) {
@@ -125,42 +141,47 @@ function ProblemList() {
 
   return (
     <div className="overflow-x-auto">
-      <div className="flex">
-        <select
-          className="select select-bordered select-sm m-2"
-          onChange={(e) => setOrder(e.target.value)}
-          value={order}
-        >
-          <option value="by-id">ID順</option>
-          <option value="by-score-desc">スコアの高い順</option>
-          <option value="by-score-asc">スコアの低い順</option>
-        </select>
-        <button
-          className="btn btn-sm"
-          onClick={() =>
-            navigator.clipboard.writeText(createScript(problems, problemKeys))
-          }
-        >
-          この順番で雑ローラースクリプトをコピー
-        </button>
-        <label className="label cursor-pointer space-x-2">
-          <span className="label-text">v1を表示 (1-55)</span>
-          <input
-            type="checkbox"
-            className="toggle toggle-primary"
-            checked={showV1}
-            onChange={(e) => setShowV1(e.target.checked)}
-          />
-        </label>
-        <label className="label cursor-pointer space-x-2">
-          <span className="label-text">v2を表示 (56+)</span>
-          <input
-            type="checkbox"
-            className="toggle toggle-primary"
-            checked={showV2}
-            onChange={(e) => setShowV2(e.target.checked)}
-          />
-        </label>
+      <div className="flex justify-between">
+        <div className="flex w-fit">
+          <select
+            className="select select-bordered select-sm m-2"
+            onChange={(e) => setOrder(e.target.value)}
+            value={order}
+          >
+            <option value="by-id">ID順</option>
+            <option value="by-score-desc">スコアの高い順</option>
+            <option value="by-score-asc">スコアの低い順</option>
+          </select>
+          <button
+            className="btn btn-sm"
+            onClick={() =>
+              navigator.clipboard.writeText(createScript(problems, problemKeys))
+            }
+          >
+            この順番で雑ローラースクリプトをコピー
+          </button>
+          <label className="label cursor-pointer space-x-2">
+            <span className="label-text">v1を表示 (1-55)</span>
+            <input
+              type="checkbox"
+              className="toggle toggle-primary"
+              checked={showV1}
+              onChange={(e) => setShowV1(e.target.checked)}
+            />
+          </label>
+          <label className="label cursor-pointer space-x-2">
+            <span className="label-text">v2を表示 (56+)</span>
+            <input
+              type="checkbox"
+              className="toggle toggle-primary"
+              checked={showV2}
+              onChange={(e) => setShowV2(e.target.checked)}
+            />
+          </label>
+        </div>
+        <div className="text-lg">
+          トータルスコア: {formatNumber(totalScore)}
+        </div>
       </div>
       <table className="table">
         <thead>
@@ -168,6 +189,7 @@ function ProblemList() {
             <th>#</th>
             <th>Image</th>
             <th>Best Score (公式)</th>
+            <th>スコア寄与率</th>
           </tr>
         </thead>
         <tbody>
@@ -178,6 +200,7 @@ function ProblemList() {
                 key={problem.id}
                 problem={problem}
                 bestSolution={bestSolutions.get(problem.id)}
+                totalScore={totalScore}
               />
             );
           })}
