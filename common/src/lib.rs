ba0949b70424c4d152bdc686e301bbd6c7f71bfc
ogiekako <ogiekako@users.noreply.github.com>
2023-07-14 11:09:29 +0000

    Apply cargo fmt

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,11 +1,11 @@
 #[cfg(not(target_arch = "wasm32"))]
 pub mod api;
 pub mod board;
+pub mod board_options;
 pub mod evaluate;
 pub mod float;
 pub mod geom;
 pub mod problem;
-pub mod board_options;
 
 pub use evaluate::*;
 pub use problem::*;
